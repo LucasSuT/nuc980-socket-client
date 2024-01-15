b605ab5705717eb7d85befc55511202c61448c11
LucasSu <LucasSu@aaeon.com.tw>
2024-01-15 17:51:16 +0800

    First UDP client

--- client.c
+++ client.c
@@ -13,9 +13,10 @@ int port = 7000;
 
 int main(int argc, char *argv[])
 {
-    int sockfd = 0, n = 0;
+    int sockfd = 0, n = 1, ret;
     char recvBuff[1024], sendBuff[1024];
     struct sockaddr_in serv_addr; 
+    socklen_t addrlen;
 
     if(argc != 2)
     {
@@ -24,11 +25,18 @@ int main(int argc, char *argv[])
     } 
 
     memset(recvBuff, '0',sizeof(recvBuff));
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
         printf("\n Error : Could not create socket \n");
         return 1;
     } 
+
+    ret = setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &n,sizeof(n));
+    if(ret < 0)
+    {
+        printf("setsockopt failed !\n");
+        return 1;
+    }
 
     memset(&serv_addr, '0', sizeof(serv_addr)); 
 
@@ -40,27 +48,35 @@ int main(int argc, char *argv[])
         printf("\n inet_pton error occured\n");
         return 1;
     } 
-
-    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-       printf("\n Error : Connect Failed \n");
-       return 1;
-    } 
-    
+    addrlen = sizeof(serv_addr);
     while(1)
     {
         printf("please input messgae: ");
 	    scanf("%s", sendBuff);
         printf("send: %s\n", sendBuff);
-	    send(sockfd, sendBuff, strlen(sendBuff), 0);
-        n = read(sockfd, recvBuff, sizeof(recvBuff)-1);
-        if(n <= 0)
+        ret = sendto(sockfd, sendBuff,strlen(sendBuff),0,(struct sockaddr *)&serv_addr,sizeof(serv_addr));
+        if(ret < 0)
         {
-            printf("\n Read error \n");
-	        break;
-    	} 
-	    recvBuff[n] = 0;
+            printf("boardcast failed !\n");
+            return 1;
+        }
+        n = recvfrom(sockfd, &recvBuff, sizeof(recvBuff), 0, (struct sockaddr*)&serv_addr, (socklen_t *)&addrlen);
+        // n = read(connfd, recvBuff, sizeof(recvBuff)-1);
+        if(n < 0)
+        {
+            printf("\n client close connection. \n");
+            break;
+        }
+        recvBuff[n] = 0;
         printf("recv: %d bytes %s\n", n, recvBuff);
+        // n = read(sockfd, recvBuff, sizeof(recvBuff)-1);
+        // if(n <= 0)
+        // {
+        //     printf("\n Read error \n");
+	    //     break;
+    	// } 
+	    // recvBuff[n] = 0;
+        // printf("recv: %d bytes %s\n", n, recvBuff);
     }
     close(sockfd);
     return 0;
